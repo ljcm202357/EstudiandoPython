41aa570be14533b6a65435d36164879247045917
Johanna Cifuentes <ljcm202381@gmail.com>
2024-02-06 09:33:55 -0500

    Ejercicio promedio numeros

---
+++ Promedio.py
@@ -0,0 +1,14 @@
+cont = 0
+Acum = 0
+
+while cont < 10:
+      Num = int(input("Ingresar el numero"))
+      Acum=Acum+Num
+
+      cont += 1
+Promedio = Acum/10
+
+print("La suma de los numeros es: ",Acum)
+print("El promedio de los 10 numeros es: ",Promedio)
+
+
